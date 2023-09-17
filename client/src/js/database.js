eb4f953eeed9676282970c503bac804dc996dffe
Joe Rivera <dvs_joe@hotmail.com>
2023-09-17 15:04:25 -0400

    fixed up some TODOs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,40 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const dbName = 'jate'; // Define the name of the database and object store.
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Function to add content to the database.
+export const putDb = async (content) => {
+  const db = await initdb();
+  const transaction = db.transaction(dbName, 'readwrite');
+  const store = transaction.objectStore(dbName);
+
+  try {
+    const id = await store.add({ content }); // Add content to the object store.
+    console.log(`Added content with ID: ${id}`);
+  } catch (error) {
+    console.error('Error adding content:', error);
+  } finally {
+    transaction.done;
+  }
+};
+
+// Function to get all content from the database.
+export const getDb = async () => {
+  const db = await initdb();
+  const transaction = db.transaction(dbName, 'readonly');
+  const store = transaction.objectStore(dbName);
+
+  try {
+    const allContent = await store.getAll();
+    console.log('Retrieved content:', allContent);
+    return allContent;
+  } catch (error) {
+    console.error('Error retrieving content:', error);
+    return [];
+  } finally {
+    transaction.done;
+  }
+};
 
 initdb();
